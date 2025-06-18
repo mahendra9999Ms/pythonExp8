a25882ec55ac574cfd0755dfbd1f3374299d4617
mahendra9999Ms <148977642+mahendra9999Ms@users.noreply.github.com>
2025-06-18 12:08:26 +0530

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,3 @@
+import secrets
+random_number=secrets.randbelow(10)+1
+print("cryptographically secure random Number:",random_number)
---
+++ uppercase.py
@@ -0,0 +1,4 @@
+import string
+text="Hello,world!"
+uppercase_text=text.upper()
+print("uppercase text:",uppercase_text)
